813c41a8b6952ae2adfb9935de0461aa79f29065
AlexanderAmpuero <ampuerosasha@gmail.com>
2025-01-25 04:52:46 -0500

    added a counter for head turns, need to relook at the heading calculations

--- main.py
+++ main.py
@@ -4,10 +4,15 @@ import numpy as np
 import time
 
 # Global variables
-score = 0
+score_body = 0
+score_head = 0
+score_arm = 0
+
 timer_active = False
 start_time = 0
 get_pos = True
+
+
 
 def calc_distance(p1: tuple, p2: tuple) -> int:
     """Takes two coordinates in (x, y, z) format and calculates the distance between them."""
@@ -25,6 +30,9 @@ class BodyTracker:
         self.pose = self.mp_pose.Pose()
         self.mp_drawing = mp.solutions.drawing_utils
         self.keypoints = {}
+        self.previous_heading = 0
+        self.previous_tilt = 0
+        self.previous_nod = 0
 
     def process_frame(self, frame):
         # Convert the frame to RGB as Mediapipe works with RGB
@@ -104,6 +112,18 @@ class BodyTracker:
         self.pose.close()
 
 
+    def count_head_turns(self, current_nod, current_roll, current_heading):
+        global score_head
+        # upper bound is the angle + 5
+        # lower bound is the angle  - 5
+        if self.previous_heading + 20 < current_heading or \
+            self.previous_tilt + 5 < current_roll < self.previous_tilt - 5 or \
+                self.previous_nod + 5 < current_nod < self.previous_nod - 5:
+            print(self.previous_heading)
+            self.previous_heading = current_heading
+            self.previous_nod = current_nod
+            self.previous_tilt = current_roll
+            score_head += 1
 
 
 if __name__ == "__main__":
@@ -143,18 +163,25 @@ if __name__ == "__main__":
                 (10, 150), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2
             )
 
+
+
             if get_pos:
                 curr_pos = metrics['hip_midpoint']
                 get_pos = False
                 timer_active = True
                 start_time = time.time()  # Start the timer
+                head_active = True
+
             
+            tracker.count_head_turns(metrics['neck_nod_angle'], metrics['neck_tilt_angle'], metrics['neck_rotation_abs'])
+            print(f'Head Score: {score_head}')
+                        
             if timer_active:
                 elapsed_time = time.time() - start_time
                 if elapsed_time >= 10:  # Timer ends after 3 seconds
                     if curr_pos - 200 <= metrics['hip_midpoint'] <= curr_pos + 200:
-                        score += 1
-                        print(f"Score: {score}")
+                        score_body += 1
+                        print(f"Score: {score_body}")
                     else:
                         print("Position moved, resetting.")
                     get_pos = True
