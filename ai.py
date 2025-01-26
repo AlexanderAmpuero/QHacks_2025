565e49f1859b847ccee57d148841790ee298a536
aampuero <ampuerosasha@gmail.com>
2025-01-26 04:23:04 -0500

    Update ai.py

--- ai.py
+++ ai.py
@@ -13,7 +13,7 @@ def get_feedback(scores):
   you are giving an overall feedback as well as areas that they could improve on. you do not need to state the values, but just give suggestions\
   rank it in what you think they need to work on most'
   client = openai.OpenAI(
-    api_key=st.secrets('OPEN_AI_API')
+    api_key=st.secrets['OPEN_AI_API']
   )
 
   completion = client.chat.completions.create(
